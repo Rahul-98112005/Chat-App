b591b911650f1d87c83202ed90c67871995471b2
Rahul <rahul98112005@gmail.com>
2025-07-06 17:25:09 +0530

    Avatar imagae Bug Fixed in RightSide Bar

--- client/src/components/RightSideBar.jsx
+++ client/src/components/RightSideBar.jsx
@@ -1,21 +1,17 @@
-import React, { useState,useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import assets, { imagesDummyData } from "../assets/assets";
 import { ChatContext } from "../../context/ChatContext";
 import { AuthContext } from "../../context/AuthContext";
 
 const RightSideBar = () => {
-
-  const { selectedUser, messages} = useContext(ChatContext)
-  const { logout, onlineUsers } = useContext(AuthContext)
-  const [msgImages, setMsgImages] = useState([])
-
+  const { selectedUser, messages } = useContext(ChatContext);
+  const { logout, onlineUsers } = useContext(AuthContext);
+  const [msgImages, setMsgImages] = useState([]);
 
   // Get all the images from the messages and set them to state
   useEffect(() => {
-    setMsgImages(
-      messages.filter(msg => msg.image).map(msg=>msg.image)
-    )
-  },[messages])
+    setMsgImages(messages.filter((msg) => msg.image).map((msg) => msg.image));
+  }, [messages]);
 
   return (
     selectedUser && (
@@ -26,12 +22,18 @@ const RightSideBar = () => {
       >
         <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
           <img
-            src={selectedUser?.profilePic || assets.avatar_icon}
+            src={
+              selectedUser?.profilePic && selectedUser.profilePic.trim() !== ""
+                ? selectedUser.profilePic
+                : assets.avatar_icon
+            }
             alt=""
             className="w-20 aspect-[1/1] rounded-full"
           />
           <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
-            {onlineUsers.includes(selectedUser._id) && <p className="w-2 h-2 rounded-full bg-green-500"></p>}
+            {onlineUsers.includes(selectedUser._id) && (
+              <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            )}
             {selectedUser.fullName}
           </h1>
           <p className="px-10 mx-auto">{selectedUser.bio}</p>
@@ -51,7 +53,10 @@ const RightSideBar = () => {
             ))}
           </div>
         </div>
-        <button onClick={() => logout()} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
+        <button
+          onClick={() => logout()}
+          className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer"
+        >
           Logout
         </button>
       </div>
